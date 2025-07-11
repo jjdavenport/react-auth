d89bc2b87990703fdbdc885d0216650e6d22c670
jjdavenport <jordanjamesdavenport@gmail.com>
2025-07-11 21:27:22 +0100

    add catch to client route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -26,6 +26,10 @@ const router = createBrowserRouter([
         path: "home",
         element: <Home />,
       },
+      {
+        path: "*",
+        element: <Navigate to="/home" />,
+      },
     ],
   },
 ]);
